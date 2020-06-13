c5f59c8f2efeda2401cf5786d1248de6e8b65be1
alirueter <rueter.alison@gmail.com>
2020-06-13 18:20:53 -0500

    code comments

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,5 @@
 // Script to create a random password based on criteria that the user decides
 
-
 //place all password criteria options into arrays
 var letterLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var letterUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
